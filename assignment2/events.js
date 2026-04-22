ccaa4beab05cc82320969178ad12900d8d6d95a9
Smeh Niki <niki.smeh@gmail.com>
2026-04-22 02:02:18 -0700

    fixed emitTimeFunction

--- assignment2/events.js
+++ assignment2/events.js
@@ -1,16 +1,14 @@
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
-
 emitter.on("time", (message) => {
   console.log("Time received: " + message);
 });
 
-
-setInterval(() => {
+const emitTime = () => {
   const currentTime = new Date().toLocaleTimeString();
   emitter.emit("time", currentTime); // Broadcast the 'time' event
-}, 5000);
+}
 
 if (require.main === module) {
   const timer = setInterval(emitTime, 5000);
